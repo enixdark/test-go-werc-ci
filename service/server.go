1b5942bf7a0b7de7f14a41ba9ef214121e017437
enixdark <cqshinn92@gmail.com>
2016-12-13 15:17:20 +0700

    fix bugs in service

--- service/server.go
+++ service/server.go
@@ -2,12 +2,14 @@ package service
 
 import (
   "net/http"
-  "github.com/urfave/negroni"
+
+  // "github.com/urfave/negroni"
+  "github.com/codegangsta/negroni"
   "github.com/gorilla/mux"
   "github.com/unrolled/render"
 )
 
-func NewServer() * negroni.Negroni{
+func NewServer()*negroni.Negroni{
   formatter := render.New(render.Options{
     IndentJSON: true,
   })
@@ -25,9 +27,8 @@ func initRoutes(mx * mux.Router, formatter *render.Render){
   mx.HandleFunc("/test", testHandler(formatter)).Methods("GET")
 }
 
-func testHandler(formatter * render.Render) http.HandleFunc{
+func testHandler(formatter * render.Render) http.HandlerFunc{
   return func(w http.ResponseWriter, req* http.Request){
-    formatter.JSON(w, http.StatusOK, 
-      struct{ Test string }{"This is a test"})
+    formatter.JSON(w, http.StatusOK, struct{ Test string }{"This is a test"})
   }
 }
