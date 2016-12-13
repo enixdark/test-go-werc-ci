71306d772e072a7c1fc297be017e9c4744971bc8
enixdark <cqshinn92@gmail.com>
2016-12-13 15:05:24 +0700

    add service package and main

--- custom-package-consumer.go
+++ custom-package-consumer.go
@@ -1,18 +1,38 @@
-package main
+// package main
 
-import(
-  "fmt"
-  "github.com/enixdark/test-go-werc-ci/npcs"
-)
+// import(
+//   // "fmt"
+//   // "github.com/enixdark/test-go-werc-ci/npcs"
+//   "fmt"
+//   "os"
 
-func main() {
-  mob := npc.NonPlayerCharacter{Name: "Alfred"}
-  fmt.Println(mob)
+//   "github.com/cloudfoundry-community/go-cfenv"
+//   service "github.com/cloudnativego/gogo-service/service"
+// )
 
-  hortense := npc.NonPlayerCharacter{Name: "hortense", 
-    Loc: npc.Location{X: 10.0, Y: 10.0, Z: 10.0}}
-  fmt.Println(hortense)
+// func main() {
+//   mob := npc.NonPlayerCharacter{Name: "Alfred"}
+//   fmt.Println(mob)
 
-  fmt.Printf("Alfred is %f units from Hortense.\n",
-  mob.DistanceTo(hortense))
-}
+//   hortense := npc.NonPlayerCharacter{Name: "hortense", 
+//     Loc: npc.Location{X: 10.0, Y: 10.0, Z: 10.0}}
+//   fmt.Println(hortense)
+
+//   fmt.Printf("Alfred is %f units from Hortense.\n",
+//   mob.DistanceTo(hortense))
+// }
+// 
+// func main() {
+//   port := os.Getenv("PORT")
+//   if len(port) == 0 {
+//     port = "3000"
+//   }
+
+//   appEnv, err := cfenv.Current()
+//   if err != nil {
+//     fmt.Println("CF Environment not detected.")
+//   }
+
+//   server := service.NewServer(appEnv)
+//   server.Run(":" + port)
+// }
